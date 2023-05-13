d8c585edfb407b64010e737881a018c2e381e809
Vic <vic.unity@skig.tech>
2023-05-13 10:21:27 +0800

    opt

--- core/models/base-model.go
+++ core/models/base-model.go
@@ -1,13 +1,12 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
 type Base struct {
-	ID        uint           `json:"id" gorm:"primarykey"` // 主键ID
-	CreatedAt time.Time      `json:"createdAt"`            // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt"`            // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
+	ID        uint       `json:"id" gorm:"primarykey"` // 主键ID
+	CreatedAt *time.Time `json:"createdAt"`            // 创建时间
+	UpdatedAt *time.Time `json:"updatedAt"`            // 更新时间
+	DeletedAt *time.Time `json:"deletedAt"`            // 删除时间
 }
--- core/models/token.go
+++ core/models/token.go
@@ -6,8 +6,9 @@ type Token struct {
 	Symbol    string `json:"symbol" gorm:"comment:符号名"`
 	Logo      string `json:"logo" gorm:"comment:LOGO"`
 	Decimals  uint8  `json:"decimals" gorm:"comment:进制"`
-	Address   string `json:"address" gorm:"comment:合约地址"`
 	Balance   string `json:"balance" gorm:"comment:余额，默认为0"`
+	Address   string `json:"address" gorm:"comment:合约地址"`
 	ChainType uint   `json:"chainType" gorm:"comment:链类型"`
 	Network   string `json:"network" gorm:"comment:网络"`
+	Sort      int    `json:"sort" gorm:"comment:排序"`
 }
